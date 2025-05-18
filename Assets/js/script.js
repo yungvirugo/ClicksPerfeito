2ca69c4adb090060cc52b328f60ce764933cd3bf
yungvirugo <yungvirugo@gmail.com>
2025-05-18 15:43:13 -0300

    sua mensagem aqui

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -23,5 +23,5 @@ const hamburger = document.getElementById('hamburger');
 const menu = document.getElementById('mobile-menu');
 
 hamburger.addEventListener('click', () => {
-  menu.style.display = menu.style.display === 'flex' ? 'none' : 'flex';
+    menu.style.display = menu.style.display === 'flex' ? 'none' : 'flex';
 });
