6c375363fc32c45d4c233329d339abdf21156800
yungvirugo <yungvirugo@gmail.com>
2025-08-07 20:42:28 -0300

    oi

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,1 +1,19 @@
+const hamburger = document.getElementById("hamburger");
+const mobileMenu = document.getElementById("mobile-menu");
+const closeMenu = document.getElementById("close-menu");
+const overlay = document.getElementById("overlay");
 
+hamburger.addEventListener("click", () => {
+  mobileMenu.classList.add("active");
+  overlay.classList.add("active");
+  document.body.style.overflow = "hidden"; // trava scroll
+});
+
+closeMenu.addEventListener("click", closeMobileMenu);
+overlay.addEventListener("click", closeMobileMenu);
+
+function closeMobileMenu() {
+  mobileMenu.classList.remove("active");
+  overlay.classList.remove("active");
+  document.body.style.overflow = "";
+}
